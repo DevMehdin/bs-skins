7af9522ffafbadb662afa3174d3f6c52f217b07a
Malkoum David <softwaredevpro2019@gmail.com>
2021-11-07 14:15:11 +0100

    Fixed config project

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -4,7 +4,7 @@ module.exports = app => {
   var router = require("express").Router();
 
   // Create a new Tutorial
-  router.post("/", tutorials.create);
+  router.post("/create", tutorials.create);
 
   // Retrieve all Tutorials
   router.get("/", tutorials.findAll);
